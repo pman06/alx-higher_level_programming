55cbeb75ff54ee90a58351e3b6966612b356ae97
Paul Shoremekun <freemeonline@gmail.com>
2022-08-23 11:31:41 -0700

    Added a script that assign a random signed number to the variable number, and print the last digit of the number stored in the variable number.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,9 +3,9 @@ import random
 number = random.randint(-10000, 10000)
 last = int(repr(number)[-1])
 if last > 5:
-    print(f"Last digit of {number:d} is  {last:d} and is greater than 5")
+    print(f"Last digit of {number:d} is {last:d} and is greater than 5")
 elif last == 0:
-    print(f"Last digit of {number:d} is  {last:d} and is 0")
+    print(f"Last digit of {number:d} is {last:d} and is 0")
 elif last < 6  and last != 0:
-    print(f"Last digit of {number:d} is  {last:d} and is less than 6 and not 0")
+    print(f"Last digit of {number:d} is {last:d} and is less than 6 and not 0")
     
